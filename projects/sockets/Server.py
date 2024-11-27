1f23d2bde428178a79b69d4559a28279c52df70d
HackExit0x0 <99713919+hackexit0x0@users.noreply.github.com>
2024-11-27 21:23:23 +0530

    Update Server.py

--- projects/sockets/Server.py
+++ projects/sockets/Server.py
@@ -6,9 +6,25 @@
 import socket
 # connetion creation (use ipv4 and Tcp)
 Connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# remove kernal sleep mode
+Connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Servre ip and port (bind)
 Connection.bind(("127.0.0.1", 8080))
 # listening
 print("[+] Listening.....")
 Connection.listen(1)
 # accept connection
+Client, address = Connection.accept()
+# print message
+print("[+] Accepted connection..")
+# send message
+
+# while loop
+while True:
+    # input shell Command
+    KernalInput = input(">> ")
+    if KernalInput.lower() == "exit":
+        break
+    Client.send(KernalInput.encode())
+    ClientOutput = Client.recv(1024).decode()
+    print(ClientOutput)
